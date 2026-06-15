a47040cddb3f16ce44c33ef048776ac2fe2a0277
tungnh2611 <nguyenhoangtung26112004@gmail.com>
2026-06-15 14:55:05 +0700

    Initial Commit

---
+++ MenstrualCycle/MenstrualCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenstrualCycleApp.swift
+//  MenstrualCycle
+//
+//  Created by Nguyen Hoang Tung on 15/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MenstrualCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
